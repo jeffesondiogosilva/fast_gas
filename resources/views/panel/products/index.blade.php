bcaba0c30edee46465ce4845f4d71e3e0a90091f
jeff <diogoworkss@gmail.com>
2025-02-25 23:28:26 -0300

    feat: organizando layout de lista de produtos

--- resources/views/panel/products/index.blade.php
+++ resources/views/panel/products/index.blade.php
@@ -1,6 +1,10 @@
 @include('layouts.app')
 
-<div class="col-md-6">
+<div class="col-md-12 text-center">
+    <h1>Lista de Produtos</h1>
+</div>
+
+<div class="col-md-12 text-right">
 
     <a href="{{ route('product.create') }}" class="btn-secondary rounded p-1">Criar Produto</a>
 
@@ -8,19 +12,19 @@
 
 <div class="col-md-12">   
     
-    <table>
+    <table class="col-md-12 pt-3 mt-3 border bg-secondary text-light">
         <thead>
-            <th class="col-md-6">Nome</th>
-            <th class="col-md-3">Preço</th>
-            <th class="col-md-3">Quantidade</th>
+            <th class="col-md-6 text-left pl-2">Nome</th>
+            <th class="col-md-3 text-center">Preço</th>
+            <th class="col-md-3 text-center">Quantidade</th>
         </thead>
 
         <tbody>
             @foreach($products as $product)
                 <tr>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->price }}</td>
-                    <td>{{ $product->quantity }}</td>
+                    <td class="text-left pl-2">{{ $product->name }}</td>
+                    <td class="text-center">{{ $product->price }}</td>
+                    <td class="text-center">{{ $product->quantity }}</td>
                 </tr>
             @endforeach
         </tbody>
